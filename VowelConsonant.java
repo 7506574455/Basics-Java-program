48f351e5107ce7e915daaa75df7051246a5e12e1
7506574455 <84278635+7506574455@users.noreply.github.com>
2021-06-18 02:50:27 -0700

    vowel or constonant

---
+++ VowelConsonant.java
@@ -0,0 +1,27 @@
+package javaPrograms;
+
+import java.util.Scanner;
+public class VowelConsonant {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter an alphabet: ");
+        char alphabet = s.next().charAt(0);
+        switch (alphabet) {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+            case 'A':
+            case 'E':
+            case 'I':
+            case 'O':
+            case 'U':
+                System.out.println(alphabet + " is vovel");
+                break;
+            default:
+                System.out.println(alphabet + " is consonant");
+        }
+
+    }
+} 
